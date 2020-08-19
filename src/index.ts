e6c1ae3f575772784a653572889830cf4f4abea3
Hauke Broer <info@owja.de>
2020-08-19 19:42:30 +0200

    add a types workaround for Intl.Locale by using the types of a polyfill

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import type {Locale as FormatJsLocale, IntlLocaleOptions} from "@formatjs/intl-locale";
+declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    namespace Intl {
+        type Locale = FormatJsLocale;
+        const Locale: {
+            new (tag?: string, options?: IntlLocaleOptions): Locale;
+        };
+    }
+}
+
 export * from "./lib/translator";
