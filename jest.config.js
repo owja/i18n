1227ee5c5a021db606a03a904fa65bb18f533a56
Hauke Broer <info@owja.de>
2020-08-28 11:33:00 +0200

    fix unit tests on node 12 by using babel

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,12 @@ module.exports = {
     coverageDirectory: "./coverage/",
     collectCoverage: true,
     coverageReporters: ["json", "lcov", "text", "clover", "html"],
+
+
+    globals: {
+        "ts-jest": {
+            babelConfig: ".babel-jest",
+        },
+    },
+
 };
